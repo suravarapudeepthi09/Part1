76d8a4e444b2e823cd0a70d25a40fe521aa6cdc7
Suravarapu Deepthi <147570119+suravarapudeepthi09@users.noreply.github.com>
2023-10-11 07:07:43 +0530

    Add files via upload

---
+++ courses.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const CourseSchema = new mongoose.Schema({
+    coursecode: String,
+    coursename: String,
+    year: Number
+});
+
+module.exports = mongoose.model('Course', CourseSchema);
